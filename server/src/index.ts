f98e5daa4a4bfdf8da6c7cb8a822f9dbba44f75a
Abstract-a <m.kamari100@gmail.com>
2024-08-28 20:24:58 +0100

    stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string).then(() => {
+  console.log('connected to db');
+});
 
 const app = express();
 
